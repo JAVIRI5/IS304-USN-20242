9a2e39f6d2d0b76128f96fac157003decdb07ed3
JAVIRI5 <edicsjaricop@unimonserrate.edu.co>
2024-09-04 20:50:44 -0500

    Tarea a realizar con dos clases más

--- SESION04/taller.py
+++ SESION04/taller.py
@@ -1,1 +1,9 @@
+'''
+Crear un programa en python que permita crear objetos de la clase CuentaBancaria, cuyos atributos son: numeroCta, nombreCliente, saldoCta, fechaApertura, ultimoRetiro, ultimaConsignacion.
+Tambien crear objetos de la clase cuenta ahorros, cuyos atributos son: cdt, sinCobro.
+Tambien crear objetos de la clase cuenta corriente, cuyos atributos son: cheques, impuestos .
+Aplicar encapsulamiento y definir los métodos apropiados para controlar y gestionar los atributos de los objetos creados.
+Utilizar un menu para las diferentes opciones, tales como crearCta, aperturaCta, consignar, retirar y transferencia entre otros.
+Y que en crear cuenta, genere que tipo de cuenta si de ahorros o corriente
+'''
 
